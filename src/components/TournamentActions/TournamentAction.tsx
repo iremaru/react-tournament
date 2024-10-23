1a9c0498ee05871a9c60b5cb106e76a82023f867
Iremaru <irenemarreroruiz@gmail.com>
2024-10-23 17:27:29 +0100

    Fix button name - end tournament

--- src/components/TournamentActions/TournamentAction.tsx
+++ src/components/TournamentActions/TournamentAction.tsx
@@ -24,7 +24,7 @@ export const TournamentAction = () => {
 				}
 				onClick={cancelTournament}
 			>
-				End tournament
+				{`${TournamentStages.TournamentEnded ? 'Restart' : 'End'} tournament`}
 			</button>
 		</div>
 	)
